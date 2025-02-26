cf373053bc95f3576fa0f52c773bedff1dad32fa
gurjas-s <gurjas35@gmail.com>
2025-02-26 13:31:56 -0800

    Updated main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from supabase import create_client, Client
 from dotenv import load_dotenv, dotenv_values
 import requests
 
+
+seasons = {"REGULAR":2, "PLAYOFFS":5}
+
 def makeDBConnection():
     load_dotenv()
     url = os.getenv("DB_URL")
@@ -16,29 +19,28 @@ The NBA's Game ID, 0021400001, is a 10-digit code: XXXYYGGGGG, where XXX refers 
 def getAllGameIds(seasonType, year):
 
     seasonDigit = 24
-    gameList = []
-    for i in range(1,1230):
-        gameId = f"00{seasonType}{seasonDigit}{i:05d}"
-        gameList.append(gameId)
     return gameList
 
-def getAllBoxScores():
-    
-    gameIds = getAllGameIds(2, 24)
+def getAllGames(seasonType, year):
+    gameList = [] #Stores all game Id's for the given season type and year
     gameRows = []
+    statRows = []
+    for i in range(1,1230):
+        gameId = f"00{seasonType}{year}{i:05d}"
+        gameList.append(gameId)
+    
     for i in range(50):
-        url = f"https://cdn.nba.com/static/json/liveData/boxscore/boxscore_{gameIds[i]}.json"  
+        url = f"https://cdn.nba.com/static/json/liveData/boxscore/boxscore_{gameList[i]}.json"  
         data = requests.get(url).json()
         if data is None:
             break
         time = data["game"]["gameTimeLocal"] 
         home = data["game"]["homeTeam"]["teamTricode"] 
         away = data["game"]["awayTeam"]["teamTricode"]
-        gameRow = {"gameId": gameIds[i], "home_team": home, "away_team":away, "game_date":time}
+        gameRow = {"gameId": gameList[i], "home_team": home, "away_team":away, "game_date":time}
         gameRows.append(gameRow) 
-        
-    
-    print(gameRows)
+         
+    return gameRows 
         
      
     
@@ -48,7 +50,8 @@ def main():
     TO DO:
         - Finish get getBoxScores 
     """
-    getAllBoxScores()
+    games, boxscores = getAllGames(seasons["REGULAR"], 24) 
+    print(games)
     #client = makeDBConnection() 
        
 
