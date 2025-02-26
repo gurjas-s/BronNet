d461d35a81ebf65d77c71d6bb8b61b457d92467e
gurjas-s <gurjas35@gmail.com>
2025-02-25 22:23:22 -0800

    Half way finished get all box scores

--- main.py
+++ main.py
@@ -9,17 +9,46 @@ def makeDBConnection():
     key = os.getenv("DB_KEY"); 
     return create_client(url,key)
 
-def getBoxScores(): 
-    url = "https://cdn.nba.com/static/json/liveData/boxscore/boxscore_0022000181.json" #Hard code for now 
-    data = requests.get(url)
-    print(data)
+"""
+The NBA's Game ID, 0021400001, is a 10-digit code: XXXYYGGGGG, where XXX refers to a season prefix, YY is the season year (e.g. 14 for 2014-15), and GGGGG refers to the game number (1-1230 for a full 30-team regular season).
+"""
+
+def getAllGameIds(seasonType, year):
+
+    seasonDigit = 24
+    gameList = []
+    for i in range(1,1230):
+        gameId = f"00{seasonType}{seasonDigit}{i:05d}"
+        gameList.append(gameId)
+    return gameList
+
+def getAllBoxScores():
+    
+    gameIds = getAllGameIds(2, 24)
+    gameRows = []
+    for i in range(50):
+        url = f"https://cdn.nba.com/static/json/liveData/boxscore/boxscore_{gameIds[i]}.json"  
+        data = requests.get(url).json()
+        if data is None:
+            break
+        time = data["game"]["gameTimeLocal"] 
+        home = data["game"]["homeTeam"]["teamTricode"] 
+        away = data["game"]["awayTeam"]["teamTricode"]
+        gameRow = {"gameId": gameIds[i], "home_team": home, "away_team":away, "game_date":time}
+        gameRows.append(gameRow) 
+        
+    
+    print(gameRows)
+        
+     
+    
 
 def main():
     """
     TO DO:
         - Finish get getBoxScores 
     """
-    getBoxScores()
+    getAllBoxScores()
     #client = makeDBConnection() 
        
 
