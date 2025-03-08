5630ffd0d6f1e82f45e999ef96329e59a81c57d3
gurjas-s <gurjas35@gmail.com>
2025-03-08 03:56:12 -0800

    fixed fetch games

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import pandas as pd
 import numpy as np
 from dotenv import load_dotenv
@@ -34,23 +35,103 @@ class NBAGamePredictor:
         self.feature_columns = None
         self.team_stats_avg = None
         
-    def fetch_data(self):
+    def fetch_data(self, key_stats):
+        def add(arr1, arr2):
+            for a in arr1:
+                arr2.append(a) 
+            
         """Fetch games and stats data from Supabase"""
-        print("Fetching data from Supabase...")
-        
+        print("Fetching data from Supabase...") 
+        page_size = 1000 
+
         # Get all games
-        games_response = supabase.table('games').select('*').execute()
+        start = page_size
+        games_response = supabase.table('games').select('*').range(0,page_size).execute()
+        games = games_response.data
+        all_games = [] 
+        add(games, all_games)
+        while(len(games)==page_size):
+            games_response = supabase.table('games').select('*').range(start,start+page_size).execute()
+            games = games_response.data
+            add(games, all_games)   
+            start = start + page_size 
         
-        games_df = pd.DataFrame(games_response.data)
 
         # Get all game stats
-        stats_response = supabase.table('gamestats').select('*').execute()
-        stats_df = pd.DataFrame(stats_response.data)
-        
+        stat_string = ','.join(key_stats)
+        select_string = 'game_id, team,' + stat_string
+        all_data = []
+        stats_response = supabase.table('gamestats').select(select_string).range(0,page_size).execute() 
+        data = stats_response.data
+        all_data.extend(data)
+        start = page_size
+        while(len(data)==page_size):
+            stats_response = supabase.table('gamestats').select(select_string).range(start,start+page_size).execute() 
+            data = stats_response.data
+            all_data.extend(data)
+            start = start+page_size 
+        #return data frames  
+        games_df, stats_df = pd.DataFrame(all_games).set_index('game_id'), pd.DataFrame(all_data).set_index(['game_id','team'])
         print(f"Fetched {len(games_df)} games and {len(stats_df)} team stat records")
-        
         return games_df, stats_df
-    
+    def prepare_features2(self, games_df, stats_df, key_stats):
+        
+        games_df['game_date'] = pd.to_datetime(games_df['game_date'])
+        games_df.sort_values('game_date', inplace=True)  
+        #merged_df = stats_df.merge(games_df, on='game_id', how='left') 
+        teams = games_df['home_team'].unique()   
+        team_averages = []
+        
+        for team in teams:
+            team_row = {}
+            team_row['team'] = team
+            team_row['count'] = 1
+            for stat in list(stats_df.columns):
+                team_row[stat] = 0
+            team_averages.append(team_row)
+        
+        team_averages_df = pd.DataFrame(team_averages)
+        prepared_list = []
+        
+        for game in games_df.itertuples(index=False):
+            home_team = game.home_team
+            away_team = game.away_team
+            home_stats = stats_df.loc[(game.game_id, home_team)] if (game.game_id, home_team) in stats_df.index else None
+            away_stats = stats_df.loc[(game.game_id, away_team)] if (game.game_id, away_team) in stats_df.index else None
+            
+            if home_stats is None or away_stats is None:
+                print("missing stats")
+                continue  
+            n, m = team_averages_df.loc[team_averages_df['team'] == home_team, 'count'].values[0], team_averages_df.loc[team_averages_df['team'] == away_team, 'count'].values[0]
+            
+            home_win = 1 if home_stats['points'] > away_stats['points'] else 0
+             
+            feature_row = {
+                'home_team':home_team, 
+                'away_team':away_team,
+                'home_win': home_win, 
+                'game_date': game['game_date']
+            }
+            for name,stat in home_stats.items():
+                if name == 'team' or name == 'game_id' or not isinstance(stat, (int, float)):
+                    continue
+                prev_home_stat = team_averages_df[team_averages_df['team']==home_team]
+                rolling_home_stat = ((n-1)/n)*prev_home_stat[name] + (1/n)*stat
+                prev_away_stat= team_averages_df[team_averages_df['team']==away_team]
+                rolling_away_stat = ((m-1)/m)*prev_away_stat[name] + (1/m)*stat
+                 
+                feature_row['home_'+name] = rolling_home_stat
+                feature_row['away_'+name] = rolling_away_stat
+
+                team_averages_df.loc[team_averages_df['team'] == home_team, name] = rolling_home_stat
+                team_averages_df.loc[team_averages_df['team'] == away_team, name] = rolling_away_stat
+            
+            team_averages_df.loc[team_averages_df['team'] == home_team, 'count'] = n+1 
+            team_averages_df.loc[team_averages_df['team'] == away_team, 'count'] = m+1 
+            prepared_list.append(feature_row)
+            print("hello")         
+        print(len(prepared_list))
+        return
     def prepare_features(self, games_df, stats_df, key_stats):
         """Prepare features for the model"""
         print("Preparing features...")
@@ -310,6 +391,12 @@ class NBAGamePredictor:
 
 def main():
     # Create predictor
+    args = sys.argv
+    save_val = False
+    if len(args) == 2:
+        if args[1] == 1:
+            save_val= True
+            
     predictor = NBAGamePredictor()
          # Define key statistics to use as features
     key_stats = [
@@ -321,9 +408,9 @@ def main():
     # Try to load existing model
     if not predictor.load_model():
         # If no model exists, fetch data and train a new one
-        games_df, stats_df = predictor.fetch_data()
-        prepared_df = predictor.prepare_features(games_df, stats_df, key_stats)
-        predictor.train_model(prepared_df,save=True)
+        games_df, stats_df = predictor.fetch_data(key_stats)
+        prepared_df = predictor.prepare_features2(games_df, stats_df, key_stats) 
+        predictor.train_model(prepared_df,save_val)
     
     # Predict upcoming games
     print("\nPredicting upcoming games:")
